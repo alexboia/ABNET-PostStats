9dcf85dab1daee0ad95c6a3f908426d03c88524a
Alexandru Boia <alexandru.boia@outlook.com>
2026-05-19 19:47:11 +0300

    Added wp filters to style metrics rendering on post edit page.

--- views/admin-style-metrics-metabox.php
+++ views/admin-style-metrics-metabox.php
@@ -18,13 +18,29 @@ if (!defined('ABSPATH')) {
 			<?php foreach($styleInfo->getMetrics() as $metric): ?>
 				<?php 
 					$isWithinBracket = $metric->isWithingBracket(); 
-					$bracketMarkerCssClass = $isWithinBracket 
+					$defaultBracketMarkerCssClass = $isWithinBracket 
 						? 'abnet-post-stats-metric-ok' 
 						: 'abnet-post-stats-metric-outside';
 
-					$bracketDescription = sprintf(__('Between %s and %s'), 
+					$bracketMarkerCssClass = apply_filters('abnet_post_stats_style_metrics_bracket_marker_css_class', 
+						$defaultBracketMarkerCssClass, 
+						$metric);
+
+					if (empty($bracketMarkerCssClass)) {
+						$bracketMarkerCssClass = $defaultBracketMarkerCssClass;
+					}
+
+					$defaultBracketDescription = sprintf(__('Between %s and %s'), 
 						$metric->getBracket()->getMin(), 
 						$metric->getBracket()->getMax());
+
+					$bracketDescription = apply_filters('abnet_post_stats_style_metrics_bracket_description', 
+						$defaultBracketDescription, 
+						$metric);
+
+					if (empty($bracketDescription)) {
+						$bracketDescription = $defaultBracketDescription;
+					}
 				?>
 				<tr class="<?php echo esc_attr($bracketMarkerCssClass) ?>">
 					<th class="row-title" style="width: 60%;" title="<?php echo esc_attr($bracketDescription); ?>"><?php echo esc_html($metric->getName()); ?></th>
